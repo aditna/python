6c3d381b80c3b9b0f4b7c0ffb5a1696dbb86a37b
aditna <75898695+aditna@users.noreply.github.com>
2020-12-12 19:53:06 +0530

    Add files via upload
    List and String Methods

---
+++ Day2_assignment.py
@@ -0,0 +1,6 @@
+name="Aditi aditi letsupgrade"
+print(name.count("aditi"))
+print(name.islower())
+print(name.isspace())
+print(name.upper())
+print(name.center(100))
---
+++ day2_listassignment.py
@@ -0,0 +1,7 @@
+lst=[1,2,3,5,'aditi','abc','abc',2.4,3.6]
+print(lst.count("abc"))
+print(lst.copy())
+print(lst.index(5))
+print(lst.pop(3))
+print(lst.clear())
+print(lst)
